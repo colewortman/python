9995f98c76ecc591eb80691566ab93f77f9af009
colewortman <wortcole12@gmail.com>
2023-08-08 17:35:07 -0500

    docstrings

--- account.py
+++ account.py
@@ -1,24 +1,49 @@
 class Account:
+    '''
+    Method to create accounts.
+    '''
     def __init__(self, name: str) -> None:
+        '''
+        Function to create account variables.
+        :param name: string variable to be used as account name.
+        '''
         self.__account_name = name
         self.__account_balance = 0
     
-    def deposit(self, amount) -> bool:
+    def deposit(self, amount: int) -> bool:
+        '''
+        Function to add to account balance by the amount
+        :param amount: integer variable to add
+        :return: boolean variable to signal transaction result
+        '''
         if not amount <= 0:
             self.__account_balance += amount
             return True
         else:
             return False
     
-    def withdraw(self, amount) -> bool:
+    def withdraw(self, amount: int) -> bool:
+        '''
+        Function to subtract to account balance by the amount
+        :param amount: integer variable to subtract
+        :return: boolean variable to signal transaction result
+        '''
         if not amount <= 0:
             self.__account_balance -= amount
             return True
         else:
             return False
     
-    def get_balance(self):
+    def get_balance(self) -> int:
+        '''
+        Method to get the account balance
+        :return: hidden account balance integer variable
+        '''
         return self.__account_balance
     
     def get_name(self) -> str:
+        '''
+        Method to get the account name
+        :return: hidden account name string variable
+        '''
         return self.__account_name
